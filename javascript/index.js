4bec8d6b3fa72f150f94a4b954199d8c4125c51f
s6sewitt <sebastian-witt@outlook.de>
2025-03-04 20:58:57 +0100

    ...

--- javascript/index.js
+++ javascript/index.js
@@ -39,7 +39,7 @@ getInstruction("mashedPotatoes", 0, (step1) => {
         getInstruction("mashedPotatoes", 4, (step5) => {
           document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
           document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`;
-//          document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
+          document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
         }, (error) => console.log(error));
       }, (error) => console.log(error));
     }, (error) => console.log(error));
@@ -79,7 +79,7 @@ obtainInstruction("steak", 0)
   .then((step7) => {
     document.querySelector("#steak").innerHTML += `<li>${step7}</li>`;
     document.querySelector("#steak").innerHTML += `<li>Stake is ready!</li>`;
-//    document.querySelector("#steakImg").removeAttribute("hidden");
+    document.querySelector("#steakImg").removeAttribute("hidden");
   })
   .catch((error) => console.log(error));
 
@@ -101,7 +101,7 @@ async function makeBroccoli() {
     const step6 = await obtainInstruction("broccoli", 6);
     document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
     document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`;
-//    document.querySelector("#broccoliImg").removeAttribute("hidden");
+    document.querySelector("#broccoliImg").removeAttribute("hidden");
   } catch (error) {
     console.log(error);
   }
